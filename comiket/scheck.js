a77c4525125faf2c83eef030df04d116b4e1ec78
yukineko <hideki030403@gmail.com>
2019-07-22 00:24:32 +0900

    update index.html

--- comiket/scheck.js
+++ comiket/scheck.js
@@ -4,67 +4,147 @@ $(document).ready(function(){
     $('.tabs').tabs()
     $('.sidenav').sidenav()
     $('select').formSelect()
+    $('.collapsible').collapsible()
+    var elem = document.querySelector('.collapsible.expandable')
+    M.Collapsible.init(elem, {
+        accordion: false
+    })
+    init()
 })
 
-/*==========================
-やること
-・買うグッズ一覧を追加できるように
-・同人グッズの価格計算
-==========================*/
-
-const island = {
-    '1': ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'ゆ', 'よ', 'ら', 'り', 'れ'],
-    '2': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'],
-    '3': ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト'],
-    '4': ['ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ']
-}
-
-// 地区変更時の島番号入れ替え
-$('#cc-list-add-ck').change(function () {
-    n = $('option:selected').val()
+// 初期化処理
+function init() {
+    $('#cc-list-add-day').empty()
     $('#cc-list-add-il').empty()
+    $('#cc-list-add-sb').empty()
+    $('#cc-list-add-ab').empty()
+
+    $('#cc-list-add-day').append('<option value="" disabled selected>日付選択</option>')
     $('#cc-list-add-il').append('<option value="" disabled selected>選択</option>')
+    $('#cc-list-add-sb').append('<option value="" disabled selected>選択</option>')
+    $('#cc-list-add-ab').append('<option value="" disabled selected>選択</option>')
 
-    for(var i = 0; island[n].length > i; i++) {
-        console.log('a')
-        $('#cc-list-add-il').append('<option value="' + (i + 1) + '">' + island[n][i] + '</option>')
+    const island = {
+        '2': ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'ゆ', 'よ', 'ら', 'り', 'れ'],
+        '1': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'],
+        '3': ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト'],
+        '4': ['ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ']
     }
-    $('#cc-list-add-il').formSelect()
-})
+    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+    const day = ['1日目 (金曜日)', '2日目 (土曜日)', '3日目 (日曜日)', '4日目 (月曜日)']
+    const table = ['a', 'b', 'ab']
 
-
-
-
-/* いらないソースコード
-
-function dateCounter() {
- 
-    var timer = setInterval(function() {
-    var nowDate = new Date()
-    var anyDate = new Date('2019/08/09 00:00:00')
-    var daysBetween = Math.ceil((anyDate - nowDate)/(1000*60*60*24))
-    var ms = (anyDate - nowDate)
-    if (ms >= 0) {
-        //時間を取得
-        var h = Math.floor(ms / 3600000);
-        var _h = h % 24;
-        //分を取得
-        var m = Math.floor((ms - h * 3600000) / 60000);
-        //秒を取得
-        var s = Math.round((ms - h * 3600000 - m * 60000) / 1000);
- 
-        //HTML上に出力
-        document.getElementById('countOutput').innerHTML = 'C96開催まで 残り' + daysBetween + '日' +_h + '時間' + m + '分' +s + '秒';
- 
-        if ((h == 0) && (m == 0) && (s == 0)) {
-        clearInterval(timer);
-        document.getElementById('countOutput').innerHTML = '経過しました'
+    for(var ii = 0; Object.keys(island).length > ii; ii++) {
+        for(var i = 0; island[ii + 1].length > i; i++) {
+            $('#cc-list-add-il').append('<option value="' + island[ii + 1][i] + '">' + island[ii + 1][i] + '</option>')
         }
-    }else{
-        document.getElementById('countOutput').innerHTML = '経過しました'
     }
-    }, 1000)
+
+    for(var i = 0; num.length > i; i++) {
+        $('#cc-list-add-sb').append('<option value="' + (i + 1) + '">' + num[i] + '</option>')
+    }
+
+    for(var i = 0; day.length > i; i++) {
+        $('#cc-list-add-day').append('<option value="' + (i + 1) + '">' + day[i] + '</option>')
+    }
+
+    for(var i = 0; table.length > i; i++) {
+        $('#cc-list-add-ab').append('<option value="' + table[i] + '">' + table[i] + '</option>')
+    }
+
+    $('#cc-list-add-sb').formSelect()
+    $('#cc-list-add-il').formSelect()
+    $('#cc-list-add-day').formSelect()
+    $('#cc-list-add-ab').formSelect()
+
+    $('#cc-list-add-circle_name').val('')
+    $('#cc-list-add-memo').val('')
 }
 
-dateCounter()
-*/
+// 保存
+$('#cc-list-add-submit').click(function() {
+    if(localStorage.getItem('circles') === null) {
+        localStorage.setItem('circles', '{}')
+    }
+
+    if($('#cc-list-add-day').val() === null || $('#cc-list-add-il').val() === null || $('#cc-list-add-sb').val() === null || $('#cc-list-add-ab').val() === null) {
+        M.toast({html: '入力内容に不備があります'})
+        return
+    }
+
+    if($('#cc-list-add-circle_name').val() === '') {
+        M.toast({html: 'サークル名が入力されていません'})
+        return
+    }
+
+    var id = $('#cc-list-add-day').val() + $('#cc-list-add-il').val() + $('#cc-list-add-sb').val() + $('#cc-list-add-ab').val()
+    var data = JSON.parse(localStorage.getItem('circles'))
+
+    if(data[id] !== undefined) {
+        data[id] = {
+            place: {
+                date: $('#cc-list-add-day').val(),
+                island: $('#cc-list-add-il').val(),
+                number: $('#cc-list-add-sb').val(),
+                ab: $('#cc-list-add-ab').val()
+            },
+            name: $('#cc-list-add-circle_name').val(),
+            memo: $('#cc-list-add-memo').val(),
+            buy: {},
+            paid: data[id].paid,
+            id: id
+        }
+        M.toast({html: '更新しました！'})
+    } else {
+        data[id] = {
+            place: {
+                date: $('#cc-list-add-day').val(),
+                island: $('#cc-list-add-il').val(),
+                number: $('#cc-list-add-sb').val(),
+                ab: $('#cc-list-add-ab').val()
+            },
+            name: $('#cc-list-add-circle_name').val(),
+            memo: $('#cc-list-add-memo').val(),
+            buy: {},
+            paid: false,
+            id: id
+        }
+        M.toast({html: '追加しました！'})
+    }
+   
+    localStorage.setItem('circles', JSON.stringify(data))
+
+    init()
+})
+
+//ホール検索
+function findHole(e) {
+// ホール検索つくる
+}
+
+//サークルリスト更新
+function updateList() {
+    var data = JSON.parse(localStorage.getItem('circles')).sort()
+    for(var i = 0; Object.keys(data).length > i; i++) {
+        tmp = data[i]
+        hole = findHole(tmp.place.island)
+        isBuy = KEY
+        buyList = []
+        memo = KEY
+        $('#cc-list-circle-wrapper').append('<li><div class="collapsible-header"><i class="material-icons" id="check-box" data-id="' + tmp.id + '">' + isBuy + '</i><span>' + tmp.id + '</span><span>' + hole + '</span><span>' + tmp.name + '</span></div><div class="collapsible-body grey lighten-4"><p>' + memo + '</p><p>' + buyList + '</p></div></li>')
+    }
+}
+
+//CheckBox
+$('#check-box').click(function() {
+    var id =  $(this).data('id')
+    var data = JSON.parse(localStorage.getItem('circles'))
+    if($(this).text() === 'check_box_outline_blank') {
+        $(this).text('check_box')
+        data[id].paid = true
+    } else {
+        $(this).text('check_box_outline_blank') 
+        data[id].paid = false
+    }
+    localStorage.setItem('circles', JSON.stringify(data))
+})
