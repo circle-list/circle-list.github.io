b0f1ccad0333eedebebf03ba451a69f89dc2c716
yukineko <hideki030403@gmail.com>
2019-12-11 17:54:56 +0900

    v0.1.0

--- startpage/src/script.js
+++ startpage/src/script.js
@@ -1,6 +1,20 @@
-VERSION = 'beta 0.0.11'
+VERSION = 'beta 0.1.0'
 
 $('#sp-version').text(VERSION)
+
+// ServiceWorker
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+    .then(function(registraion) {
+        console.log('[Service Worker] Registed.')
+        registraion.update();
+        serviceWorkerUpdatefoundRefreshDialog.register(registraion, {
+          message: '更新があります。ページを再読み込みしてください。',
+          //onClick: location.reload()
+      })
+    })
+}
 
 function getStr(d) {
     if(localStorage.getItem(d) === null) {
---
+++ startpage/sw.js
@@ -0,0 +1,69 @@
+// メインはこっちを変える
+// サービスに直接的に関わる変更など
+var CACHE_STATIC_VERSION = 'static-v1.0'
+
+// こっちはassets系統のアプデ時のみ使用
+// フォント変更やmaterialize,jQueryなどの外部ライブラリ更新時のみ変更
+var CACHE_DYNAMIC_VERSION = 'dynamic-v1.0'
+
+importScripts("https://unpkg.com/service-worker-updatefound-refresh-dialog@1.1.0/dist/service-worker-updatefound-refresh-dialog.umd.js");
+
+// 以下メイン処理
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker...')
+  event.waitUntil(
+    caches.open(CACHE_STATIC_VERSION)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App...')
+        cache.addAll([
+          '/comiket/',
+          '/comiket/manifest.json',
+          '/comiket/src/style.css',
+          '/comiket/src/scheck.js',
+          '/comiket/src/map.css',
+          '/comiket/src/reg_sw.js'
+        ])
+      }).then(function() {
+        skipWaiting()
+      }))
+  
+})
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ...')
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone())
+                  return res
+                })
+            })
+            .catch(function() {
+            })
+        }
+      })
+  )
+})
+
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker...')
+    event.waitUntil(
+      caches.keys()
+        .then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+            if (key !== CACHE_STATIC_VERSION && key !== CACHE_DYNAMIC_VERSION) {
+              console.log('[Service Worker] Removing old cache...')
+              return caches.delete(key)
+            }
+          }))
+        })
+    )
+    return self.clients.claim()
+  })
