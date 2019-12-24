5f60a44850a98d0bacb54c6ccf30fd4300311a08
yukineko <hideki030403@gmail.com>
2019-12-24 22:33:46 +0900

    sw.js update

--- comiket/sw.js
+++ comiket/sw.js
@@ -4,7 +4,7 @@ var CACHE_STATIC_VERSION = 'static-v2.14'
 
 // こっちはassets系統のアプデ時のみ使用
 // フォント変更やmaterialize,jQueryなどの外部ライブラリ更新時のみ変更
-var CACHE_DYNAMIC_VERSION = 'dynamic-v3'
+var CACHE_DYNAMIC_VERSION = 'dynamic-v3.1'
 
 importScripts("https://unpkg.com/service-worker-updatefound-refresh-dialog@1.1.0/dist/service-worker-updatefound-refresh-dialog.umd.js");
 
