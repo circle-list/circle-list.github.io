ff67f533b77a73016094b7dd3fb977401b224d92
yukineko <hideki030403@gmail.com>
2020-01-15 21:16:12 +0900

    v2.43

--- comiket/src/scheck.js
+++ comiket/src/scheck.js
@@ -1286,6 +1286,10 @@ function checkQuery() {
                 }
             
                 localStorage.setItem('circles', JSON.stringify(data))
+
+                setTimeout(function() {
+                    location.reload()
+                }, 500)
             } catch(err) {
                 M.toast({html: '追加に失敗しました'})
                 console.log(err)
