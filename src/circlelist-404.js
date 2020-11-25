9fc127fe0a2e877f5c566c6d8a5b7a0d9d9349bc
yukineko <hideki030403@gmail.com>
2020-11-25 21:08:21 +0900

    v3

--- src/circlelist-404.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    console.log('(* ╹ヮ╹*) < ココには何もないよ！')
-    setTimeout(function() {
-        location.href = 'index.html'
-    }, 5000)
-})
--- src/devtools.js
+++
@@ -1,1 +0,0 @@
-var d=JSON.parse(localStorage.getItem('config'));if(d!==null&&d['enable-devtools']){eruda.init()}
